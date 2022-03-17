3466ce80a4731ba61f8eee8d906941da553e89f0
Suresh Manga <Suresh.Manga@digital.homeoffice.gov.uk>
2022-03-17 11:33:27 +0000

    [eahw-1586] Added helper class for constants

---
+++ jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/utils/ConstantHelper.java
@@ -0,0 +1,13 @@
+package uk.gov.homeoffice.digital.sas.jparest.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ConstantHelper {
+
+    public static final String URL_ID_PATH_PARAM =  "/{id}";
+    public static final String URL_RELATED_ID_PATH_PARAM =  "/{related_id}";
+    public static final String apiRootPath = "/resources";
+    public static final String PATH_DELIMITER = "/";
+
+}
