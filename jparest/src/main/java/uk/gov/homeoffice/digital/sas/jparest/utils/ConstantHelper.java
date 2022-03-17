feac8ae963ee420aae338e836a927927bd483b1c
velmurugan muthu <velmurugan.muthu@digital.homeoffice.gov.uk>
2022-03-17 17:55:13 +0000

    EAHW-1586 : Fixed the Sonar Critical issue

--- jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/utils/ConstantHelper.java
+++ jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/utils/ConstantHelper.java
@@ -7,7 +7,7 @@ public class ConstantHelper { //NOSONAR
 
     public static final String URL_ID_PATH_PARAM =  "/{id}";
     public static final String URL_RELATED_ID_PATH_PARAM =  "/{related_id}";
-    public static final String apiRootPath = "/resources";
+    public static final String API_ROOT_PATH = "/resources";
     public static final String PATH_DELIMITER = "/";
 
 }
