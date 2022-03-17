76e86548ef5dc8f2aa5334a687262aae7d8832f9
Suresh Manga <Suresh.Manga@digital.homeoffice.gov.uk>
2022-03-17 15:59:01 +0000

    [eahw-1586] fix code smells

--- jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/utils/ConstantHelper.java
+++ jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/utils/ConstantHelper.java
@@ -3,7 +3,7 @@ package uk.gov.homeoffice.digital.sas.jparest.utils;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class ConstantHelper { // NOSONAR
+public class ConstantHelper { //NOSONAR
 
     public static final String URL_ID_PATH_PARAM =  "/{id}";
     public static final String URL_RELATED_ID_PATH_PARAM =  "/{related_id}";
