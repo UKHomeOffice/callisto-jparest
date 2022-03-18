211794bb3cac8be094b14689c8d87b5a364fbc71
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-03-18 11:50:25 +0000

    Removed code that came from tutorial and cleaned up the sql and data

--- demo/src/main/java/uk/gov/homeoffice/digital/sas/demo/repositories/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.homeoffice.digital.sas.demo.repositories;
-
-import uk.gov.homeoffice.digital.sas.demo.models.Session;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-    
-}
