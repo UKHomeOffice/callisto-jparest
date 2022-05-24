e565b9f720510920b684a3dfc21694d459452e99
velmurugan muthu <velmurugan.muthu@digital.homeoffice.gov.uk>
2022-05-24 16:13:59 +0100

    Removing ID fields validation in BaseEntity as it is done in entityUtils class

--- jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/models/BaseEntity.java
+++ jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/models/BaseEntity.java
@@ -1,22 +1,13 @@
 package uk.gov.homeoffice.digital.sas.jparest.models;
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
-import org.hibernate.proxy.HibernateProxy;
-import uk.gov.homeoffice.digital.sas.jparest.exceptions.ResourceException;
 
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.NotNull;
-import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -39,33 +30,6 @@ public abstract class BaseEntity {
     @Setter
     @Type(type="uuid-char")
     private UUID tenantId;
-
-    /**
-     * Disabling this more than one Id field validation as it is done now in Entityutils as part of EAHW-1851,
-     * TODO: remove the commented code after the discussion with team
-     * @return {@link Field} annotated with {@link Id}
-
-    @NotNull
-    // S3011: Need to be able to read id field for equals and hashcode
-    // S3958: ToList is not identified as a terminal operator
-    // https://community.sonarsource.com/t/s3958-stream-tolist-jdk-16-is-not-recognized-as-terminal-operator/41501
-    // Suppresion can be removed when our sonar is updated
-    @SuppressWarnings({"squid:S3011", "squid:S3958"})
-    private Field getIdField() {
-        var entityClass = this instanceof HibernateProxy ? this.getClass().getSuperclass() : this.getClass();
-        List<Field> idFields = Arrays
-                .stream(entityClass.getDeclaredFields())
-                .filter(e -> e.isAnnotationPresent(Id.class))
-                .toList();
-        if (idFields.size() > 0) {
-            throw new ResourceException(String.format(ID_ERROR_MESSAGE, BaseEntity.class.getName(),
-                    entityClass.getName(), idFields.size()+1));
-        }
-        idFields.get(0).setAccessible(true);
-        return idFields.get(0);
-    }
-     */
-
 
     @Override
     public boolean equals(Object o) {
