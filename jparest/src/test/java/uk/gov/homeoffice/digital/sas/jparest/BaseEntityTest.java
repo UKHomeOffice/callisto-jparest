bc311ce7b1df302926d248be6d27d6ebfb36a44a
Velmurugan <velmurugan.muthu@digital.homeoffice.gov.uk>
2022-06-09 16:47:15 +0100

    review comments implemented to use id field for equals instead of identifier reference

--- jparest/src/test/java/uk/gov/homeoffice/digital/sas/jparest/BaseEntityTest.java
+++ jparest/src/test/java/uk/gov/homeoffice/digital/sas/jparest/BaseEntityTest.java
@@ -25,7 +25,7 @@ class BaseEntityTest {
     public static final UUID SAMPLE2_TENANT_ID = UUID.fromString("7a7c7da4-bb29-11ec-8422-0242ac120002");
 
     @Test
-    void getIdField_classContainsSingleIdAnnotatedField_noExceptionThrown() {
+    void getIdField_classContainsSingleIdField_noExceptionThrown() {
         assertThatNoException().isThrownBy(DummyEntityA::new);
     }
 
@@ -64,7 +64,7 @@ class BaseEntityTest {
 
     @ParameterizedTest(name="{0}")
     @MethodSource("equalObjects")
-    void equals_calledOnDifferentInstancesOfSameTypeWithMatchingIdAnnotatedFields_returnsTrue(Object object1, Object object2) {
+    void equals_calledOnDifferentInstancesOfSameTypeWithMatchingIdFields_returnsTrue(Object object1, Object object2) {
         assertThat(object1).isEqualTo(object2);
     }
 
@@ -99,13 +99,18 @@ class BaseEntityTest {
     static Stream<Arguments> equalObjects() {
         // Objects with the same ID field
         var g1 = new GuidIdEntity();
-        g1.setId(UUID.randomUUID());
+        g1.setId(SAMPLE_ID);
         g1.setTenantId(SAMPLE_TENANT_ID);
 
         var g2 = new GuidIdEntity();
+        g2.setId(SAMPLE_ID);
         g2.setTenantId(SAMPLE_TENANT_ID);
-        g2.setId(g1.getId());
-        
+
+        // Objects with the same ID field & different tenantid value
+        var gt2 = new GuidIdEntity();
+        g2.setId(SAMPLE_ID);
+        g2.setTenantId(SAMPLE2_TENANT_ID);
+
         // Objects the same ID field
         var a1 = new DummyEntityA();
         a1.setId(SAMPLE_ID);
@@ -116,6 +121,7 @@ class BaseEntityTest {
 
         return Stream.of(
             Arguments.of(named("same id field value", g1), g2),
+            Arguments.of(named("same id & different tenantid value", g1), gt2),
             Arguments.of(named("same instance", g1), g1),
             Arguments.of(named("same instance", g2), g2),
             Arguments.of(named("same id field value", a1), a2),
@@ -133,27 +139,27 @@ class BaseEntityTest {
         g2.setId(UUID.randomUUID());
         g2.setTenantId(SAMPLE_TENANT_ID);
 
-        // Same value in id field with null ID field
+        // with null id field
         var g3 = new GuidIdEntity();
-        g3.setIdentifier(SAMPLE_ID);
+        g3.setId(null);
         g3.setTenantId(SAMPLE_TENANT_ID);
         var g4 = new GuidIdEntity();
-        g4.setIdentifier(SAMPLE_ID);
+        g4.setId(null);
         g4.setTenantId(SAMPLE_TENANT_ID);
 
-        // Same value in id field with non-null ID field
+        // with non-null id field
         var g5 = new GuidIdEntity();
-        g5.setIdentifier(UUID.randomUUID());
         g5.setId(SAMPLE_ID);
         g5.setTenantId(SAMPLE_TENANT_ID);
-        
+        g5.setIdentifier(UUID.randomUUID());
+
         // Same value in description field but null ID field
         var c1 = new DummyEntityC();
         c1.setDescription("something");
         var c2 = new DummyEntityC();
         c2.setDescription("something");
 
-        // Same ID annotated field value but different types
+        // Same id field value but different types
         var c3 = new DummyEntityC();
         c3.setId(SAMPLE_ID);
         c3.setTenantId(SAMPLE_TENANT_ID);
@@ -165,9 +171,9 @@ class BaseEntityTest {
             Arguments.of(named("different ids", g1), g2),
             Arguments.of(named("null id field", g3), g4),
             Arguments.of(named("null id  field != non null id  field", g4), g5),
-            Arguments.of(named("non null id  field != null id  field", g4), g5),
+            Arguments.of(named("non null id  field != null id  field", g5), g4),
             Arguments.of(named("null id field", c1), c2),
-            Arguments.of(named("same id field but different types", c3), a1),
+            Arguments.of(named("same id field but different entity types", c3), a1),
             Arguments.of(named("right side null object", c3), null),
             Arguments.of(named("left side null object", null), c3)
         );
