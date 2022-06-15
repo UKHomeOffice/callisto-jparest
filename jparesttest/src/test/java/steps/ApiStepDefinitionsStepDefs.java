51bef2c045a58e680ae7676f82b5031e1d1035d9
charlieashton <charlie.ashton@2itesting.com>
2022-06-15 12:18:46 +0100

    cleanup

--- jparesttest/src/test/java/steps/ApiStepDefinitionsStepDefs.java
+++ jparesttest/src/test/java/steps/ApiStepDefinitionsStepDefs.java
@@ -1,19 +1,12 @@
 package steps;
 
 import io.cucumber.java.en.Then;
-import net.thucydides.core.annotations.Steps;
-import stepLib.ApiActions;
-
 import java.util.LinkedList;
-
 import static org.mockito.Mockito.*;
 
 public class ApiStepDefinitionsStepDefs {
 
     LinkedList mockedList = mock(LinkedList.class);
-
-    @Steps
-    private static ApiActions apiActions;
 
     @Then("^I call the \"([^\"]*)\" \"([^\"]*)\" endpoint with \"([^\"]*)\" and parameter \"([^\"]*)\"$")
     public void request(String requestType, String endpoint, String bearerToken, String param) throws InterruptedException {
