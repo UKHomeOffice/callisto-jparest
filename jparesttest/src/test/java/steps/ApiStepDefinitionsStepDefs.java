5db8d519b72db3b824b55301e9858f19d3905e1e
charlieashton <charlie.ashton@2itesting.com>
2022-06-16 09:23:21 +0100

    updated pom

--- jparesttest/src/test/java/steps/ApiStepDefinitionsStepDefs.java
+++ jparesttest/src/test/java/steps/ApiStepDefinitionsStepDefs.java
@@ -1,19 +1,14 @@
 package steps;
 
 import io.cucumber.java.en.Then;
-import java.util.LinkedList;
-import static org.mockito.Mockito.*;
 
 public class ApiStepDefinitionsStepDefs {
-
-    LinkedList mockedList = mock(LinkedList.class);
 
     @Then("Test GET call")
     public void request() {
         System.out.println("****************");
         System.out.println("hello");
         System.out.println("****************");
-
     }
 
 }
