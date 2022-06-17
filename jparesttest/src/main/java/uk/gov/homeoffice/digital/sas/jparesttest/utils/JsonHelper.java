572e427de3ba5878c15938f39b56bd31912e9496
charlieashton <charlie.ashton@2itesting.com>
2022-06-17 13:27:34 +0100

    updated put request feature

--- jparesttest/src/main/java/uk/gov/homeoffice/digital/sas/jparesttest/utils/JsonHelper.java
+++ jparesttest/src/main/java/uk/gov/homeoffice/digital/sas/jparesttest/utils/JsonHelper.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
@@ -15,31 +14,11 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
 public class JsonHelper {
 
     public static final String ENTITY_ID_VALUE = "9999999999";
-    public static String VALUE_REPLACEMENT = "replace0";
 
     public static String createJsonObject(final String fileName, final String entityIdValue) {
         try {
             String payload = new String(Files.readAllBytes(Paths.get(fileName)));
             return payload.contains(ENTITY_ID_VALUE) ? payload.replace(ENTITY_ID_VALUE, entityIdValue) : payload;
-        } catch (IOException e) {
-            log.error("Error While reading file: {} - {}", fileName, e.getMessage());
-        }
-        return EMPTY;
-    }
-
-    public static String createJpaRestApiJsonObjectWithManipulation(final String fileName, final List<String> entityIdValue) {
-        try {
-            String payload = new String(Files.readAllBytes(Paths.get(fileName)));
-            for (int i = 0; i <= entityIdValue.size(); ) {
-                if (payload.contains(VALUE_REPLACEMENT)) {
-                    payload = payload.replace(VALUE_REPLACEMENT, entityIdValue.get(i));
-                }
-                int replaceIndex = i + 1;
-                VALUE_REPLACEMENT = VALUE_REPLACEMENT.replace("replace" + i, "replace" + replaceIndex);
-                i++;
-            }
-            VALUE_REPLACEMENT = "replace0";
-            return payload;
         } catch (IOException e) {
             log.error("Error While reading file: {} - {}", fileName, e.getMessage());
         }
