dae15a3772182a07a8f9dc7347301cf8bd40210d
Jonathan-Weston1 <jonathan.weston@digital.homeoffice.gov.uk>
2022-08-16 17:00:20 +0100

    dev/eahw-2109/jparest-library : Update non maintained docker image with mvn extraction

--- git_tag.sh
+++ git_tag.sh
@@ -1,8 +1,11 @@
+version=0.0.1
+
 mkdir /root/.ssh/ && echo "$SSH_KEY" > /root/.ssh/id_rsa && chmod 0600 /root/.ssh/id_rsa
 ssh-keyscan github.com >> /root/.ssh/known_hosts && chmod 600 /root/.ssh/known_hosts
 printf  "Host github.com\n   Hostname github.com\n   IdentityFile /root/.ssh/id_rsa\n" > /root/.ssh/config
 chmod 0600 /root/.ssh/config
 git remote add tag-origin  git@github.com:UKHomeOffice/callisto-jparest.git
-version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 git tag $version
 git push tag-origin $version
+
+
