9118cc344f7fc64bd7c851bd50b28687a31e7332
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2022-10-28 11:31:09 +0100

    uncomment test

--- jparest/src/test/java/uk/gov/homeoffice/digital/sas/jparest/entityutils/testentities/DummyEntityA.java
+++ jparest/src/test/java/uk/gov/homeoffice/digital/sas/jparest/entityutils/testentities/DummyEntityA.java
@@ -1,34 +1,34 @@
-//package uk.gov.homeoffice.digital.sas.jparest.entityutils.testentities;
-//
-//import java.util.Set;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.JoinTable;
-//import javax.persistence.ManyToMany;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import uk.gov.homeoffice.digital.sas.jparest.annotation.Resource;
-//import uk.gov.homeoffice.digital.sas.jparest.models.BaseEntity;
-//
-//@Resource(path = "dummyEntityAs")
-//@Entity(name = "dummyEntityA")
-//public class DummyEntityA extends BaseEntity {
-//
-//    @Getter
-//    @Setter
-//    @ManyToMany
-//    @JoinTable(name = "dummy_EntityA_dummy_EntityB", joinColumns = @JoinColumn(name = "dummy_EntityA"), inverseJoinColumns = @JoinColumn(name = "dummy_EntityB"))
-//    private Set<DummyEntityB> dummyEntityBSet;
-//
-//    @Getter
-//    @Setter
-//    private Long profileId;
-//
-//    @Getter
-//    @Setter
-//    private Long index;
-//
-//}
+package uk.gov.homeoffice.digital.sas.jparest.entityutils.testentities;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+import uk.gov.homeoffice.digital.sas.jparest.annotation.Resource;
+import uk.gov.homeoffice.digital.sas.jparest.models.BaseEntity;
+
+@Resource(path = "dummyEntityAs")
+@Entity(name = "dummyEntityA")
+public class DummyEntityA extends BaseEntity {
+
+    @Getter
+    @Setter
+    @ManyToMany
+    @JoinTable(name = "dummy_EntityA_dummy_EntityB", joinColumns = @JoinColumn(name = "dummy_EntityA"), inverseJoinColumns = @JoinColumn(name = "dummy_EntityB"))
+    private Set<DummyEntityB> dummyEntityBSet;
+
+    @Getter
+    @Setter
+    private Long profileId;
+
+    @Getter
+    @Setter
+    private Long index;
+
+}
