e5f846a14cfa94a50f4e5c24c8e5e7e6e034acbc
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-11-08 16:46:42 +0000

    Add javadoc to string generator class

--- cucumber-jparest/src/main/java/uk/gov/homeoffice/digital/sas/cucumberjparest/utils/SecureRandomStringGenerator.java
+++ cucumber-jparest/src/main/java/uk/gov/homeoffice/digital/sas/cucumberjparest/utils/SecureRandomStringGenerator.java
@@ -9,12 +9,20 @@ public class SecureRandomStringGenerator {
   static final String ALPHABET_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   private static final SecureRandom secureRandom = new SecureRandom();
 
-  public static String randomAlphabetic(int count) {
-    return randomAlphabetic(count, ALPHABET_SET);
+  public static String randomAlphabetic(int size) {
+    return random(size, ALPHABET_SET);
   }
 
-  public static String randomAlphabetic(int count, String characterBase) {
-    return secureRandom.ints(count, 0, characterBase.length())
+  /**
+   * Securely generate a random set of characters of size {@code size}, using {@code characterBase}
+   * as a base.
+
+   * @param size size of generated string
+   * @param characterBase base of characters used to generate the random string
+   * @return random string of the specified size, using the specified character base
+   */
+  public static String random(int size, String characterBase) {
+    return secureRandom.ints(size, 0, characterBase.length())
         .mapToObj(ALPHABET_SET::charAt)
         .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
         .toString();
