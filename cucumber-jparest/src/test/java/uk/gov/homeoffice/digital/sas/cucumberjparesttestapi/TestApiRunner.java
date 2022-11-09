22e5a7e6bc7f8924cefd6b999df41431831be501
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-11-09 13:01:33 +0000

    PR review feedback: fix indentation

--- cucumber-jparest/src/test/java/uk/gov/homeoffice/digital/sas/cucumberjparesttestapi/TestApiRunner.java
+++ cucumber-jparest/src/test/java/uk/gov/homeoffice/digital/sas/cucumberjparesttestapi/TestApiRunner.java
@@ -20,13 +20,12 @@ public class TestApiRunner {
         @Bean
         public OpenAPI customOpenAPI(@Value("1.0.0.0") String appVersion) {
                 return new OpenAPI()
-                                .info(new Info()
-                                                .title("Example API")
-                                                .version(appVersion)
-                                                .description(
-                                                                "This is a sample API server created using springdocs - a library for OpenAPI 3 with spring boot.")
-                                                .termsOfService("http://swagger.io/terms/")
-                                                .license(new License().name("Apache 2.0")
-                                                                .url("http://springdoc.org")));
+                    .info(new Info()
+                    .title("Example API")
+                    .version(appVersion)
+                    .description("This is a sample API server created using springdocs - a library for OpenAPI 3 with spring boot.")
+                    .termsOfService("http://swagger.io/terms/")
+                    .license(new License().name("Apache 2.0")
+                    .url("http://springdoc.org")));
         }
 }
