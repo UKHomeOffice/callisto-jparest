337c57185696963e4c0a0cb5c877af1d5ec8a6f7
claire-guest <claire.guest@digital.homeoffice.gov.uk>
2022-11-14 16:10:15 +0000

    fix sonar code smells

--- jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/exceptions/ResourceConstraintViolationException.java
+++ jparest/src/main/java/uk/gov/homeoffice/digital/sas/jparest/exceptions/ResourceConstraintViolationException.java
@@ -1,14 +1,14 @@
 package uk.gov.homeoffice.digital.sas.jparest.exceptions;
 
-import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 public class ResourceConstraintViolationException extends RuntimeException {
 
   @Getter
-  private final ArrayList<StructuredError> errorResponse;
+  private final List<StructuredError> errorResponse;
 
-  public ResourceConstraintViolationException(ArrayList<StructuredError> o) {
+  public ResourceConstraintViolationException(List<StructuredError> o) {
     this.errorResponse = o;
   }
 
