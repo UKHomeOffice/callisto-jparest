b66b9cb87be130a01f5ca71dbd83c9ea911d90d2
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2022-11-21 19:50:04 +0000

    moved the logic to build the service registry map where it really belongs: the ServiceRegistry class

--- cucumber-jparest/src/main/java/uk/gov/homeoffice/digital/sas/cucumberjparest/config/JpaTestContext.java
+++ cucumber-jparest/src/main/java/uk/gov/homeoffice/digital/sas/cucumberjparest/config/JpaTestContext.java
@@ -1,13 +1,10 @@
 package uk.gov.homeoffice.digital.sas.cucumberjparest.config;
 
-import static uk.gov.homeoffice.digital.sas.cucumberjparest.api.ServiceRegistry.SERVICE_REGISTRY_SYSTEM_PROPERTY_NAME;
-
 import io.cucumber.spring.ScenarioScope;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +16,6 @@ import uk.gov.homeoffice.digital.sas.cucumberjparest.api.ServiceRegistry;
 import uk.gov.homeoffice.digital.sas.cucumberjparest.persona.PersonaManager;
 import uk.gov.homeoffice.digital.sas.cucumberjparest.scenarios.ScenarioState;
 import uk.gov.homeoffice.digital.sas.cucumberjparest.scenarios.interpolation.Interpolation;
-import uk.gov.homeoffice.digital.sas.cucumberjparest.utils.SerialisationUtil;
 
 /**
  * Class used by the {Link ContextConfiguration} annotation to configure an
@@ -38,11 +34,6 @@ public class JpaTestContext {
       Map.class.getSimpleName(), Map.class,
       List.class.getSimpleName(), List.class,
       Instant.class.getSimpleName(), Instant.class);
-  private static final String SERVICE_REGISTRY_SYSTEM_PROPERTY_EL
-      = "#{systemProperties['" + SERVICE_REGISTRY_SYSTEM_PROPERTY_NAME + "']}";
-
-  @Value(SERVICE_REGISTRY_SYSTEM_PROPERTY_EL)
-  private String serviceRegistryString;
 
   /**
    * The service registry needs to be accessed to by the Cucumber context and the test runner so
@@ -50,8 +41,7 @@ public class JpaTestContext {
    */
   @Bean
   public ServiceRegistry serviceRegistry() {
-    Map<String, String> servicesMap = SerialisationUtil.stringToMap(serviceRegistryString);
-    return new ServiceRegistry(servicesMap);
+    return new ServiceRegistry();
   }
 
   /**
