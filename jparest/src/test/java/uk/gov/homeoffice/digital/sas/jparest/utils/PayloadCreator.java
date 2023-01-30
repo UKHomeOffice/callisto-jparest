767aae141175597bd0c9bab6bf4563cc3cf0d3ca
laurie.bennett <laurie.bennett@digital.homeoffice.gov.uk>
2023-01-30 10:06:41 +0000

    EAHW-2546 improved PayloadCreator method to account for multiple line payloads

--- jparest/src/test/java/uk/gov/homeoffice/digital/sas/jparest/utils/PayloadCreator.java
+++ jparest/src/test/java/uk/gov/homeoffice/digital/sas/jparest/utils/PayloadCreator.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 public class PayloadCreator {
 
-    public static String createIDPayload(List<String> fieldName, List<String> fieldValue){
+    public static String createIDPayload(List<String> fieldNameList, List<String> fieldValueList){
         StringBuilder payload = new StringBuilder("{\n");
-        for (int i = 0; i < fieldName.size(); i++){
-            payload.append("\"").append(fieldName.get(i)).append("\": \"").append(fieldValue.get(i)).append("\"");
+        for (int i = 0; i < fieldNameList.size() && i < fieldValueList.size(); i++){
+            payload.append("\"").append(fieldNameList.get(i)).append("\": \"").append(fieldValueList.get(i)).append("\"");
+            if(i != fieldNameList.size()-1 && i!= fieldNameList.size()-1){
+                payload.append(",\n");
+            }
         }
         payload.append("}");
         return payload.toString();
