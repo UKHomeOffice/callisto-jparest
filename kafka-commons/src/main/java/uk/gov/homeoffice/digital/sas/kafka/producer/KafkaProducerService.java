7361e942ff2404c3fdb53b0d0b0fd2f712a0764e
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2023-02-20 14:11:52 +0000

    PR feedback

--- kafka-commons/src/main/java/uk/gov/homeoffice/digital/sas/kafka/producer/KafkaProducerService.java
+++ kafka-commons/src/main/java/uk/gov/homeoffice/digital/sas/kafka/producer/KafkaProducerService.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import uk.gov.homeoffice.digital.sas.kafka.message.KafkaAction;
 import uk.gov.homeoffice.digital.sas.kafka.message.KafkaEventMessage;
 
-@Component
+@Service
 @EnableAutoConfiguration
 public class KafkaProducerService<T> {
 
@@ -28,7 +28,7 @@ public class KafkaProducerService<T> {
   @SuppressWarnings("unchecked")
   public void sendMessage(@NotNull String messageKey, @NotNull T resource, KafkaAction action) {
     var kafkaEventMessage = new KafkaEventMessage<>(projectVersion,
-        (Class<T>)resource.getClass(), resource, action);
+        (Class<T>) resource.getClass(), resource, action);
     kafkaTemplate.send(topicName, messageKey, kafkaEventMessage);
   }
 }
