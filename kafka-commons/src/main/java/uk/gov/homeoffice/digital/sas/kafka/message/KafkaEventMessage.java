99756120570b763b09b6548d23b5cc6c5b08660a
Taoufik Mohdit <91465696+taomoh@users.noreply.github.com>
2023-02-23 11:10:31 +0000

    Add KafkaEventMessage default constructor required by Object Mapper for deserialisation. Also add equals and hashcode methods for facilitate comparison (#56)
    Co-authored-by: Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>

--- kafka-commons/src/main/java/uk/gov/homeoffice/digital/sas/kafka/message/KafkaEventMessage.java
+++ kafka-commons/src/main/java/uk/gov/homeoffice/digital/sas/kafka/message/KafkaEventMessage.java
@@ -1,20 +1,24 @@
 package uk.gov.homeoffice.digital.sas.kafka.message;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class KafkaEventMessage<T> {
 
   public static final String SCHEMA_FORMAT = "%s, %s";
 
-  private final String schema;
+  private String schema;
 
   @NotNull
-  private final T resource;
+  private T resource;
 
   @NotNull
-  private final KafkaAction action;
+  private KafkaAction action;
 
   public KafkaEventMessage(String projectVersion, Class<T> resourceType, T resource,
       KafkaAction action) {
