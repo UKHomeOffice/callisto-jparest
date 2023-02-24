766f5e9877dd4a1ced788e109cad12806977c8c7
Taoufik Mohdit <91465696+taomoh@users.noreply.github.com>
2023-02-24 10:57:04 +0000

    Revert 56 eahw 2577/add kafka message default constructor (#57)
    * Revert "Add KafkaEventMessage default constructor required by Object Mapper for deserialisation. Also add equals and hashcode methods for facilitate comparison (#56)"
    
    This reverts commit 99756120570b763b09b6548d23b5cc6c5b08660a.
    
    * Bump up version
    
    ---------
    
    Co-authored-by: Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>

--- kafka-commons/src/main/java/uk/gov/homeoffice/digital/sas/kafka/message/KafkaEventMessage.java
+++ kafka-commons/src/main/java/uk/gov/homeoffice/digital/sas/kafka/message/KafkaEventMessage.java
@@ -1,24 +1,20 @@
 package uk.gov.homeoffice.digital.sas.kafka.message;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@EqualsAndHashCode
 public class KafkaEventMessage<T> {
 
   public static final String SCHEMA_FORMAT = "%s, %s";
 
-  private String schema;
+  private final String schema;
 
   @NotNull
-  private T resource;
+  private final T resource;
 
   @NotNull
-  private KafkaAction action;
+  private final KafkaAction action;
 
   public KafkaEventMessage(String projectVersion, Class<T> resourceType, T resource,
       KafkaAction action) {
