f42102341c109d23fbfc48176f61c605922048b6
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-04-03 11:45:17 +0100

    Change TearDown method to camelCase

--- kafka-commons/src/test/java/uk/gov/homeoffice/digital/sas/kafka/consumer/KafkaConsumerServiceImpl.java
+++ kafka-commons/src/test/java/uk/gov/homeoffice/digital/sas/kafka/consumer/KafkaConsumerServiceImpl.java
@@ -31,11 +31,14 @@ public class KafkaConsumerServiceImpl implements ConsumerSeekAware {
   @Value("${spring.kafka.template.default-topic}")
   private String topic;
 
-  @KafkaListener(topics = { "${spring.kafka.template.default-topic}" }, groupId = "${spring.kafka.consumer.group-id}")
+  @KafkaListener(
+      topics = { "${spring.kafka.template.default-topic}" },
+      groupId = "${spring.kafka.consumer.group-id}")
   public void onMessage(@Payload String message) {
     if (latch == null) {
-      throw new NullPointerException("Message recieved before the expected number of messages had been set." +
-          " Ensure setExpectedNumberOfMessages has been called.");
+      throw new NullPointerException(
+          "Message recieved before the expected number of messages had been set."
+          + " Ensure setExpectedNumberOfMessages has been called.");
     }
     if (latch.getCount() == 1) {
       kafkaEventMessage = kafkaConsumerService.consume(message);
@@ -48,7 +51,7 @@ public class KafkaConsumerServiceImpl implements ConsumerSeekAware {
     this.callback = callback;
   }
 
-  public void TearDown() {
+  public void tearDown() {
     latch = null;
     kafkaEventMessage = null;
     callback.seekToEnd(topic, 0);
@@ -60,8 +63,8 @@ public class KafkaConsumerServiceImpl implements ConsumerSeekAware {
 
   public boolean awaitMessages(long timeout, TimeUnit unit) {
     if (latch == null) {
-      throw new NullPointerException("awaitMessages can not be called before the expected number" +
-          " of messages has been set. Ensure setExpectedNumberOfMessages has been called.");
+      throw new NullPointerException("awaitMessages can not be called before the expected number"
+          + " of messages has been set. Ensure setExpectedNumberOfMessages has been called.");
     }
     boolean latchCompleted = false;
     try {
