4eba504ded87b4d4688b44737529f9c2c3483399
JWeston-HO <jonathan.weston@digital.homeoffice.gov.uk>
2023-04-14 12:24:07 +0100

    Add KafkaConsumerUtilsTest class

---
+++ kafka-commons/src/test/java/uk/gov/homeoffice/digital/sas/kafka/consumer/KafkaConsumerUtilsTest.java
@@ -0,0 +1,79 @@
+package uk.gov.homeoffice.digital.sas.kafka.consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.homeoffice.digital.sas.Constants.TestConstants.KAFKA_VALID_VERSION;
+import static uk.gov.homeoffice.digital.sas.Constants.TestConstants.SCHEMA_FIELD_VALUE;
+import static uk.gov.homeoffice.digital.sas.kafka.constants.Constants.KAFKA_DESERIALIZATION_TO_CONCRETE_TYPE_FAILED;
+import static uk.gov.homeoffice.digital.sas.kafka.constants.Constants.KAFKA_SUCCESSFUL_DESERIALIZATION;
+import static uk.gov.homeoffice.digital.sas.kafka.constants.Constants.RESOURCE_JSON_ATTRIBUTE;
+import static uk.gov.homeoffice.digital.sas.kafka.consumer.KafkaConsumerUtils.getResourceFromMessageAsString;
+import static uk.gov.homeoffice.digital.sas.kafka.consumer.KafkaConsumerUtils.getSchemaFromMessageAsString;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.time.LocalDateTime;
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.util.ObjectUtils;
+import uk.gov.homeoffice.digital.sas.config.TestConfig;
+import uk.gov.homeoffice.digital.sas.kafka.message.KafkaAction;
+import uk.gov.homeoffice.digital.sas.model.Profile;
+import uk.gov.homeoffice.digital.sas.utils.TestUtils;
+
+@SpringBootTest(classes = TestConfig.class)
+@ExtendWith(OutputCaptureExtension.class)
+class KafkaConsumerUtilsTest {
+
+  @Autowired
+  KafkaConsumerUtils<Profile> consumerUtils;
+
+  private static final Long PROFILE_ID = 1L;
+  private static final String TENANT_ID = "tenantId";
+  private static final String PROFILE_NAME = "Original profile";
+  private final static Date START_TIME = TestUtils.getAsDate(LocalDateTime.now());
+
+  @Test
+  void checkDeserializedResource_logsSuccessWhenValidPayload(CapturedOutput capturedOutput) {
+    Profile profile = new Profile(PROFILE_ID, TENANT_ID, PROFILE_NAME, START_TIME);
+    consumerUtils.checkDeserializedResource("resource", profile);
+
+    assertThat(capturedOutput.getOut()).contains(String.format(KAFKA_SUCCESSFUL_DESERIALIZATION,
+        "resource"));
+  }
+
+  @Test
+  void checkDeserializedResource_logsFailureWhenInvalidPayload(CapturedOutput capturedOutput) {
+    Profile profile = null;
+    consumerUtils.checkDeserializedResource("resource", profile);
+
+    assertThat(capturedOutput.getOut()).contains(String.format(KAFKA_DESERIALIZATION_TO_CONCRETE_TYPE_FAILED,
+        "resource"));
+  }
+
+  @Test
+  void getSchemaFromMessageAsString_returnsSchemaAsResource() {
+    String payload = TestUtils.createKafkaMessage(KAFKA_VALID_VERSION);
+
+    assertThat(getSchemaFromMessageAsString(payload))
+        .isEqualTo(String.format(SCHEMA_FIELD_VALUE, KAFKA_VALID_VERSION));
+
+  }
+
+  @Test
+  void getResourceFromMessageAsString_returnMessageAsString() {
+    String payload = TestUtils.createKafkaMessage(KAFKA_VALID_VERSION);
+
+    JsonObject jsonMessage = JsonParser.parseString(payload).getAsJsonObject();
+    jsonMessage.get(RESOURCE_JSON_ATTRIBUTE).toString();
+
+    assertThat(getResourceFromMessageAsString(payload))
+        .isEqualTo(jsonMessage.get(RESOURCE_JSON_ATTRIBUTE).toString());
+  }
+
+}
